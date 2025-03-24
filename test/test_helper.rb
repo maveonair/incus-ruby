9015c9677f54187283932e62f910cedfe7736eb9
Fabian Mettler <dev@maveonair.com>
2025-03-24 21:51:06 +0100

    Configure VCR for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "incus"
 
 require "minitest/autorun"
+
+# Order matters!
+require "httpx"
+require "vcr"
+require "webmock"
+require "httpx/adapters/webmock"
+require "webmock/minitest"
+
+require "incus"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/casettes"
+  config.hook_into(:webmock)
+end
